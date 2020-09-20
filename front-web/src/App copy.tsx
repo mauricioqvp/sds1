07587805e4c76948e242064f050f10bae2bd2fc1
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-09-19 23:07:49 -0300

    Adicionando componete Header

---
+++ front-web/src/App copy.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <Hello message="Washington" />
+      <Hello message="Nélio" />
+      <Hello message="World" />
+      <Hello message="semana DevSuperior" />
+    </div>
+  );
+}
+
+export default App;
