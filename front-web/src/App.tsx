eadb684319e448a267eadfff9fbc6597a044b52d
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-09-20 03:16:21 -0300

    Criando pagina de listagem dos registros

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
-import './App.css';
 import Routes from './Routers';
 
 function App() {
