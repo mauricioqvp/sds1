cb21dd71d8a7a5938d746224f5d9e3938c15e10f
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-12-21 10:13:33 -0300

    Picker funcioando corretamente

--- front-mobile/src/pages/CreateRecord/index.tsx
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesome5 as Icon } from '@expo/vector-icons';
 import { StyleSheet, View, TextInput } from 'react-native';
 import Header from '../../components/Header';
 import PlatformCard from './PlatformCard';
-import { GamePlatform } from './types';
+import { GamePlatform, Game } from './types';
 import RNPickerSelect from 'react-native-picker-select';
+import axios from 'axios';
 
 const placeholder = {
   label: 'Selecione o game',
   value: null
 }
 
+const BASE_URL = 'http://192.168.1.3:8080';
+
+const mapSelectValues = (games: Game[]) => {
+  return games.map(game => ({
+    ...game,
+    label: game.title,
+    value: game.id
+  }));
+}
+
 const CreateRecord = () => {
 
   const [platform, setPlatform] = useState<GamePlatform>();
   const [selectedGame, setSelectedGame] = useState('');
+  const [allGames, setAllGames] = useState<Game[]>([]);
+  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
 
   const handleChangePlatform = (selectedPlatform: GamePlatform) => {
     setPlatform(selectedPlatform);
+    const gamesByPlatform = allGames.filter(
+      game => game.platform === selectedPlatform
+    )
+      setFilteredGames(gamesByPlatform);
   }
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/games`)
+      .then(response => {
+        const selectValues = mapSelectValues(response.data);
+        console.log(selectValues);
+        setAllGames(selectValues);
+      })
+  }, []);
 
   return (
     <>
@@ -36,40 +62,37 @@ const CreateRecord = () => {
           placeholderTextColor="#9E9E9E"
           maxLength={3}
         />
-      <View style={styles.platformContainer}>
-        <PlatformCard
-          platform="PC"
-          icon="laptop"
-          onChange={handleChangePlatform}
-          activePlatform={platform}
-        />
-        <PlatformCard
-          platform="XBOX"
-          icon="xbox"
-          onChange={handleChangePlatform}
-          activePlatform={platform}
-        />
-        <PlatformCard
-          platform="PLAYSTATION"
-          icon="playstation"
-          onChange={handleChangePlatform}
-          activePlatform={platform}
-        />
+        <View style={styles.platformContainer}>
+          <PlatformCard
+            platform="PC"
+            icon="laptop"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+          <PlatformCard
+            platform="XBOX"
+            icon="xbox"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+          <PlatformCard
+            platform="PLAYSTATION"
+            icon="playstation"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
         </View>
         <RNPickerSelect
-        onValueChange={value => {
-          setSelectedGame(value);
-        }}
+          onValueChange={value => {
+            setSelectedGame(value);
+          }}
           placeholder={placeholder}
-          items={[
-            { label: 'Football', value: 'football' },
-            { label: 'Baseball', value: 'baseball' },
-            { label: 'Hockey', value: 'hockey' },
-        ]}
-        style={pickerSelectStyles}
-        Icon={() => {
-           return <Icon name="chevron-down" color="#9E9E9E" size={25} />
-        }}
+          value={selectedGame}
+          items={filteredGames}
+          style={pickerSelectStyles}
+          Icon={() => {
+            return <Icon name="chevron-down" color="#9E9E9E" size={25} />
+          }}
         />
       </View>
     </>
